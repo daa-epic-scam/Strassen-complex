b9a641de5c35d106425d86a92b7a7e9f1bb874bf
ThisAbhinav <this.abhinav26@gmail.com>
2024-03-25 15:34:22 +0530

    asdad

--- complex.cpp
+++ complex.cpp
@@ -1,35 +1,42 @@
 #include <iostream>
 #include "complex.h"
 
-Complex Complex::split(std::string cnum) {
-    float real = 0; // PLACEHOLDER 
+Complex Complex::split(std::string cnum)
+{
+    float real = 0; // PLACEHOLDER
     float im = 0;
     return Complex(real, im);
 }
 
-std::string Complex::format() {
+std::string Complex::format()
+{
     return std::to_string(this->r) + " + " + std::to_string(this->i) + "i";
 }
 
-Complex::Complex(float r, float i) {
+Complex::Complex(float r, float i)
+{
     this->r = r;
     this->i = i;
 }
 
-Complex::Complex(std::string cnum) {
+Complex::Complex(std::string cnum)
+{
     Complex c = split(cnum);
     this->r = c.r;
     this->i = c.i;
 }
 
-Complex Complex::operator+(Complex& addend) {
-    return Complex(this->r + addend.r, this->i + addend.i);
+Complex Complex::operator+(Complex &factor)
+{
+    return Complex(this->r + factor.r, this->i + factor.i);
 }
 
-Complex Complex::operator*(Complex& addend) {
-    return Complex(this->r * addend.r - this->i * addend.i, this->i * addend.r + this->r * addend.i);
+Complex Complex::operator*(Complex &factor)
+{
+    return Complex(this->r * factor.r - this->i * factor.i, this->i * factor.r + this->r * factor.i);
 }
 
-void Complex::print() {
+void Complex::print()
+{
     std::cout << this->format() << std::endl;
 }
--- complex.h
+++ complex.h
@@ -1,6 +1,7 @@
 #include <iostream>
 
-class Complex {
+class Complex
+{
 private:
     float r;
     float i;
@@ -8,18 +9,17 @@ private:
     Complex split(std::string); // converts string to complex number
                                 // have to import libs to return multiple vals
                                 // so returning the object itself
-    
+
 public:
     std::string format(); // converts complex number to string
 
     Complex(float r, float i); // constructor 1
 
     Complex(std::string); // constructor 2 for ease
-    
-    Complex operator+(Complex& addend);
-    
-    Complex operator*(Complex& addend);
 
-    
+    Complex operator+(Complex &factor);
+
+    Complex operator*(Complex &factor);
+
     void print();
 };
