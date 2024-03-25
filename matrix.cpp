38bf8d4f58b034823243750f343f7b97863b7ef0
Chaitanya Modi <143248164+Chaitanya-modi@users.noreply.github.com>
2024-03-25 17:58:55 +0530

    matrix intialization

--- matrix.cpp
+++ matrix.cpp
@@ -1,20 +1,62 @@
 #include <iostream>
-#include "complex.h"
 
-template <class T>
-class Matrix {
-    int rows;
-    int cols;
-    T **matrix;
+class Complex
+{
+public:
+    double real, imag;
 
-    public:
-    Matrix(int rows, int cols, T values) {
-        this->rows = rows;
-        this->cols = cols;
-        this->matrix = new T*[rows];
+    Complex(double r = 0.0, double i = 0.0) : real(r), imag(i) {}
 
-        for (int i = 0; i < rows; i++) {
-            this->matrix[i] = new T[cols];
-        }
+    Complex operator+(const Complex &other) const
+    {
+        return Complex(real + other.real, imag + other.imag);
     }
+
+    Complex operator-(const Complex &other) const
+    {
+        return Complex(real - other.real, imag - other.imag);
+    }
+
+    Complex operator*(const Complex &other) const
+    {
+        return Complex(real * other.real - imag * other.imag, real * other.imag + imag * other.real);
+    }
+};
+
+class Matrix
+{
+public:
+    // constructor for number of rows and columns in matrix
+    Matrix(size_t rows = 0, size_t cols = 0) : rows_(rows), cols_(cols), data_(new Complex[rows * cols]) {}
+
+    size_t rows() const
+    {
+        return rows_;
+    }
+
+    size_t cols() const
+    {
+        return cols_;
+    }
+
+    // access elements in matrix
+    Complex &at(size_t row_index, size_t col_index)
+    {
+        if (row_index >= rows_ || col_index >= cols_)
+        {
+            throw std::out_of_range("Indices out of range");
+        }
+        return data_[row_index * cols_ + col_index];
+    }
+
+    // destructor
+    ~Matrix()
+    {
+        delete[] data_;
+    }
+
+private:
+    size_t rows_;
+    size_t cols_;
+    Complex *data_;
 };
